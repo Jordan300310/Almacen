d7e3976feb0d26123097e16081f2a4a4d73e8608
Jordan300310 <jordan.014569@gmail.com>
2025-09-11 12:35:08 -0500

    Tercer Commit

--- backend/api/empresa_get.php
+++ backend/api/empresa_get.php
@@ -6,10 +6,10 @@ require_login(); // cualquiera logueado puede leer
 
 json_ok([
   'empresa' => [
-    'nombre'    => 'ARQ Almacén',
+    'nombre'    => 'Casa Concepto Muebles Perú',
     'ruc'       => '20123456789',
     'direccion' => 'Av. Siempre Viva 123 - Lima',
     'telefono'  => '(01) 555-1234',
-    'email'     => 'ventas@arqalmacen.com'
+    'email'     => 'ventas@gmail.com'
   ]
 ]);
